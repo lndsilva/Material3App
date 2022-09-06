a484df09c764c3142d1d093131424a436530a642
lndsilva <lndsilva@hotmail.com>
2022-09-06 11:56:39 -0300

    Initial commit

---
+++ app/src/main/java/br/com/local/material3app/Produtos_Activity.java
@@ -0,0 +1,14 @@
+package br.com.local.material3app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Produtos_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.produtos_layout);
+    }
+}
